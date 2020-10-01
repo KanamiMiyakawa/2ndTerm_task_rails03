24b15238dcfeab1e929c210b6952bf3d585ad29d
kanami miyakawa <kedamap@yahoo.co.jp>
2020-10-01 06:42:41 +0000

    update routes users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :blogs
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create, :show] do
+    member do
+      get :favorites
+    end
+  end
 end
