26c0b9a75f3ecb88abb1a21a053ae8674c3b1bae
kanami miyakawa <kedamap@yahoo.co.jp>
2020-10-01 08:07:35 +0000

    update SessionsController and helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  include SessionsHelper
+
+  def authenticate_user
+    if current_user == nil
+      flash[:notice] = t('notice.login_needed')
+      redirect_to new_session_path
+    end
+  end
+
+  def fobid_login_user
+    if current_user
+      flash[:notice] = "すでにログインしています"
+      redirect_to blogs_path
+    end
+  end
 end
