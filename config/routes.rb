777ed84d2c0d4dd641c3a03fa1dac1a34c28a393
kanami miyakawa <kedamap@yahoo.co.jp>
2020-10-01 07:58:59 +0000

    update sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+
   resources :blogs
+
   resources :users, only: [:new, :create, :show] do
     member do
       get :favorites
     end
   end
+
+  resources :sessions, only: [:new, :create, :destroy]
+
 end
