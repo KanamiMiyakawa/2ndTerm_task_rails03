844ef0e8821272af35a3f3819b302f233d15367d
kanami miyakawa <kedamap@yahoo.co.jp>
2020-10-01 06:30:48 +0000

    update user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  validates :name,  presence: true, length: { maximum: 30 }
+
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: true,
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+  mount_uploader :image, ImageUploader
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+
+  has_many :blogs
+
+  before_validation { email.downcase! }
 end
