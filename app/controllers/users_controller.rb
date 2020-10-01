62634295bc30bb4e310828adb0338a1514e5e6ca
kanami miyakawa <kedamap@yahoo.co.jp>
2020-10-01 08:09:30 +0000

    update login_user restriction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :favorites]
+  before_action :fobid_login_user, only: [:new, :create]
 
   def new
     @user = User.new
