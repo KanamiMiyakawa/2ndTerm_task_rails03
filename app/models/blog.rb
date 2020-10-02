cadd21676e4b8369b1c705e418c5908871fa765d
kanami miyakawa <kedamap@yahoo.co.jp>
2020-10-02 07:28:51 +0000

    update locele setting

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,11 @@
 class Blog < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
+  validates :content_or_picture, presence: true
 
   mount_uploader :picture, PictureUploader
+
+  def content_or_picture
+    content.presence or picture.presence
+  end
 end
