49031b9c95bc89df7b1fcff731367e3db2295907
kanami miyakawa <kedamap@yahoo.co.jp>
2020-10-02 08:28:06 +0000

    update helpers

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,15 +2,15 @@ module BlogsHelper
   def choose_index_or_edit
     if action_name == 'index' || action_name == 'create' || action_name == 'confirm'
       confirm_blogs_path
-    elsif action_name == 'edit'
+    elsif action_name == 'edit' || action_name == 'update'
       blog_path
     end
   end
 
   def submit_button_value
-    if action_name == "index" || 'create' || 'confirm'
+    if action_name == 'index' || action_name == 'create' || action_name == 'confirm'
       "確認画面へ"
-    elsif action_name == "edit"
+    elsif action_name == 'edit' || action_name == 'update'
       "投稿する"
     end
   end
---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,19 @@
+module UsersHelper
+
+  def user_new_or_edit
+    if action_name == 'new' || action_name == 'create'
+      users_path
+    elsif action_name == 'edit' || action_name == 'update'
+      user_path
+    end
+  end
+
+  def user_submit_button_value
+    if action_name == 'new' || action_name == 'create'
+      "Create your account!"
+    elsif action_name == "edit" || action_name == 'update'
+      "Update your account!"
+    end
+  end
+
+end
