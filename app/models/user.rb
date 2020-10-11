06517dbabb3561358d776e6a5ad74cfd6b6d7eab
kanami miyakawa <kedamap@yahoo.co.jp>
2020-10-11 08:30:37 +0000

    add following-followed association

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,11 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_blogs, through: :favorites, source: :blog
 
+  has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
+  has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+
   before_validation { email.downcase! }
 end
